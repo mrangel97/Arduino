61bfdc798bb598bd2b79111b5c6d10a95776ff5a
Marina Rangel <marina.anapurus@hotmail.com>
2019-06-26 02:58:18 -0300

    Update display.ino

--- display.ino
+++ display.ino
@@ -4,11 +4,10 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <DHT.h>
-
 // Definicao do pino do sensor 
 #define DHTPIN 2
 // Tipo de sensor
-#define DHTTYPE DHT22
+#define DHTTYPE 22
 
 // Definicao de pinos interface SPI
 #define OLED_SDA   9
@@ -21,18 +20,16 @@ Adafruit_SSD1306 display(OLED_SDA, OLED_SCK, OLED_DC, OLED_RESET, OLED_CS);
 // Criacao do objeto para comunicacao com o sensor
 DHT dht(DHTPIN, DHTTYPE);
 
-// Piezoeletrico
+//Piezoeletrico
 const int piezo = 0; // A0
-const int limiarLimite = 50; // BATIMENTO
+const int limiarLimite = 50; // Batimento
 
 void setup()   
 {
   Serial.begin(9600);
   // Inicializa o display
   display.begin(SSD1306_SWITCHCAPVCC);
-  // Inicializa o sensor DHT22
   dht.begin();
-  // Apaga o display
   display.clearDisplay();
 }
 
@@ -41,21 +38,24 @@ void loop()
   float umidade = dht.readHumidity(); // ler umidade
   float temperatura = dht.readTemperature(); // ler temperatura
 
-  display.setTextSize(2); //tamanho do texto
+  display.setTextSize(1); //tamanho do texto
   display.setTextColor(WHITE);
   display.print("Umidade: "); // mostra a umidade
   display.print(umidade);
-  display.print(" %");
+  display.println(" %");
   display.print("Temperatura: "); // mostra a temperatura
   display.print(temperatura);
-  display.print(" *C");
+  display.println(" *C");
   display.display(); //executa o texto no display
-
+  
   int val = analogRead(piezo);
-    if (val >= limiarLimite)
-    {
-        display.println("TOC!TOC");
-        display.display();
-    }
-    delay(1000);
+  if (val >= limiarLimite)
+  {
+    display.clearDisplay();
+    display.setCursor(10,10);
+    display.setTextSize(2);
+    display.println("TOC!TOC!");
+    display.display();
+    delay(5000);
+  }
 }
