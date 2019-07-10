33e39210ce973807a0ac7bf80d2ad29e74294d3e
Marina Rangel <marina.anapurus@hotmail.com>
2019-07-10 01:47:37 -0300

    Update button_display.ino

--- button_display.ino
+++ button_display.ino
@@ -1,17 +1,21 @@
-// Nesse projeto é utilizado o sensor de temperatura e umidade DHT22,o piezoelétrico e o push button
+// Nesse projeto é utilizado o sensor de temperatura e umidade DHT22 e o push button
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <DHT.h>
+
 // Definicao do pino do sensor 
-#define DHTPIN 2
+#define DHTPIN 3
 // Tipo de sensor
 #define DHTTYPE 22
 
+//Definição do pino do push button
+#define BUTTON 2
+
 // Definicao de pinos interface SPI
-#define OLED_SDA   9
-#define OLED_SCK   10
+#define OLED_SDA   9 
+#define OLED_SCK   10 
 #define OLED_DC    11
 #define OLED_CS    12
 #define OLED_RESET 13
@@ -20,54 +24,40 @@ Adafruit_SSD1306 display(OLED_SDA, OLED_SCK, OLED_DC, OLED_RESET, OLED_CS);
 // Criacao do objeto para comunicacao com o sensor
 DHT dht(DHTPIN, DHTTYPE);
 
-//Piezoeletrico
-const int piezo = 0; // A0
-const int limiarLimite = 50; // Batimento
+void umid_temp(){
+  float umidade = dht.readHumidity();
+  float temperatura = dht.readTemperature();
 
-//Push Button
-int pressao = 0;
+  display.setCursor(10 , 10);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
 
-void setup()   
-{
+  display.print("Umidade: ");
+  display.print(umidade);
+  display.println(" %");
+
+  display.print("Temperatura: ");
+  display.print(temperatura);
+  display.println(" *C");
+
+  display.display();
+}
+
+void setup() {
   Serial.begin(9600);
-  // Inicializa o display
   display.begin(SSD1306_SWITCHCAPVCC);
   dht.begin();
   display.clearDisplay();
-  pinMode(3,INPUT); // Push button
+  pinMode(BUTTON , INPUT);
 }
 
-void loop() 
-{
-  pressao = digitalRead(3);
-  if(pressao == HIGH)
-  {
-      float umidade = dht.readHumidity(); // ler umidade
-      float temperatura = dht.readTemperature(); // ler temperatura
-    
-      display.setTextSize(1); //tamanho do texto
-      display.setTextColor(WHITE);
-      display.print("Umidade: "); // mostra a umidade
-      display.print(umidade);
-      display.println(" %");
-      display.print("Temperatura: "); // mostra a temperatura
-      display.print(temperatura);
-      display.println(" *C");
-      display.display(); //executa o texto no display
-     
-      int val = analogRead(piezo);
-      if (val >= limiarLimite)
-      {
-        display.clearDisplay();
-        display.setCursor(10,10);
-        display.setTextSize(2);
-        display.println("TOC!TOC!");
-        display.display();
-        delay(5000);
-      }
+void loop() {
+  int pressao = digitalRead(BUTTON);
+  Serial.println(pressao);
+  if(pressao == 1){
+    umid_temp(); 
   }
-  else
-  {
+  else if(pressao == 0){
     display.clearDisplay();
   }
 }
