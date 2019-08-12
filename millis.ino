79b9d020d437343627127b0081d1f2eaa0a8dd29
Marina Rangel <marina.anapurus@hotmail.com>
2019-08-12 01:05:06 -0300

    Create millis.ino

---
+++ millis.ino
@@ -0,0 +1,78 @@
+// Nesse código,através da função millis,o display será ligado com o push button sendo pressionado por 1 segundo ou mais que isso
+// E o mesmo para desligar o display
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
+// Definicao do pino do sensor 
+#define DHTPIN 3
+// Tipo de sensor
+#define DHTTYPE 22
+
+//Definição do pino do push button
+#define BUTTON 2
+
+// Definicao de pinos interface SPI
+#define OLED_SDA   9 
+#define OLED_SCK   10 
+#define OLED_DC    11
+#define OLED_CS    12
+#define OLED_RESET 13
+Adafruit_SSD1306 display(OLED_SDA, OLED_SCK, OLED_DC, OLED_RESET, OLED_CS);
+
+// Criacao do objeto para comunicacao com o sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned long time_atual = 0;
+unsigned long time_ult = 0;
+
+void umid_temp(){
+  float umidade = dht.readHumidity();
+  float temperatura = dht.readTemperature();
+
+  display.setCursor(10 , 10);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  display.print("Umidade: ");
+  display.print(umidade);
+  display.println(" %");
+
+  display.print("Temperatura: ");
+  display.print(temperatura);
+  display.println(" *C");
+
+  display.display();
+}
+
+void setup() {
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC);
+  dht.begin();
+  display.clearDisplay();
+  pinMode(BUTTON , INPUT);
+}
+
+void loop() {
+  int pressao = digitalRead(BUTTON);
+  time_atual = millis();
+  if(pressao == 0){
+     time_ult = time_atual;
+  }
+  if((pressao == 1) && (time_atual - time_ult >= 1000)){
+       umid_temp();
+       time_ult = 0;
+       time_atual = 0;
+       pressao = digitalRead(BUTTON);
+       time_atual = millis();
+       if(pressao == 0){
+          umid_temp();
+       }
+       if((pressao == 1) && (time_atual - time_ult >= 1000)){
+           display.clearDisplay();
+           display.display();
+       }
+  }  
+}
