d8776ee58f060450952649343cc62de322de1c65
Marina Rangel <marina.anapurus.mr@gmail.com>
2019-12-09 11:41:45 -0300

    Update led1seg.ino

--- led1seg.ino
+++ led1seg.ino
@@ -5,6 +5,7 @@ unsigned long tempo_atual = 0;
 int estadoLed = 0;
 const int LED = 13;
 const int BUTTON = 3;
+int jaPiscou = 0;
 
 
 void setup() {
@@ -16,21 +17,20 @@ void loop() {
    int pressionado = digitalRead(BUTTON);
    tempo_atual = millis();
    if(pressionado) {
-       if (tempo_atual - ultimo_tempo >= 1000){
+       if ((tempo_atual - ultimo_tempo >= 1000) && (jaPiscou == 0)){
            estadoLed = !estadoLed;
            ultimo_tempo = tempo_atual;
-       }
-       else if(tempo_atual - ultimo_tempo >= 2000){
-           digitalWrite(LED, LOW);
        }
        if(estadoLed == 1){
           digitalWrite(LED, HIGH);
        }
        else if(estadoLed == 0){
           digitalWrite(LED, LOW);
+          jaPiscou = 1;
        }
    }
    else if(!pressionado){
          ultimo_tempo = tempo_atual;
+         jaPiscou = 0;
    }
 }
